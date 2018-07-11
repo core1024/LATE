f57b8a2d462ff71915638821c220fc568773e91f
Vladimir Borisov <borisov1024@gmail.com>
2018-07-11 21:32:47 +0300

    Reorder games EEPROM save. Fixes

--- arduino-mini-games.ino
+++ arduino-mini-games.ino
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include <Arduboy2.h>
+#include <EEPROM.h>
 
 #include "buttons.h"
 #include "block.h"
 
-#include "game_bgun.h"
 #include "game_tetris.h"
+#include "game_bgun.h"
 #include "game_snake.h"
 
 #define BTN_PIN_UP 9
@@ -20,14 +21,14 @@ Arduboy2 arduboy;
 
 struct game_t {
   const __FlashStringHelper *name;
-  char *file;
+  int address;
   void (*play)(Arduboy2 *sgr, uint8_t *gdat, uint8_t menu, uint8_t *gameOn, uint32_t *score, uint32_t *hiScore);
 };
 
 const uint8_t game_data_sz = 240;
 uint8_t game_data[game_data_sz];
 
-const uint8_t games_count = 4;
+const uint8_t games_count = 3;
 struct game_t games[games_count];
 
 uint8_t rot = 1;
@@ -37,74 +38,70 @@ uint8_t choice = 0;
 uint8_t fh;
 
 void setup() {
-  games[0].name = F("CTPElbA");
-  games[0].file = (char *)"BGUN";
-  games[0].play = &gameBGun;
-  games[1].name = F("TETPiC");
-  games[1].file = (char *)"TETRIS";
-  games[1].play = &gameTetris;
-  games[2].name = F("zMiq");
-  games[2].file = (char *)"SNAKE";
+  games[0].name = F("TETRIS");
+  games[0].address = EEPROM_STORAGE_SPACE_START + sizeof(game_data);
+  games[0].play = &gameTetris;
+  games[1].name = F("BGUN");
+  games[1].address = EEPROM_STORAGE_SPACE_START;
+  games[1].play = &gameBGun;
+  games[2].name = F("SNAKE");
+  games[2].address = EEPROM_STORAGE_SPACE_START + 2 * sizeof(game_data);
   games[2].play = &gameSnake;
 
-  buttonsSetup(&arduboy);
   arduboy.begin();
   arduboy.setFrameRate(30);
+  blocksSetup(&arduboy);
+  buttonsSetup(&arduboy);
 
-  fh = BLOCK_LINE;
+  fh = 8;
 }
 
 void loop() {
   uint32_t score, hiScore;
   uint8_t menu;
   uint8_t game_on = 0;
-  uint8_t btns = 1, i;
+  uint8_t i;
 
   while (!game_on) {
     if (!arduboy.nextFrame()) {
       continue;
     }
     arduboy.pollButtons();
-        arduboy.drawRect(0, 0, WIDTH, HEIGHT);
-        arduboy.setCursor(2, fh - 4);
-        arduboy.print(F("MEHu"));
-        arduboy.drawFastHLine(0, fh + 2, WIDTH);
+    arduboy.clear();
 
-        arduboy.setCursor(2, fh * choice + fh + fh - 1);
-        arduboy.print(">");
+    arduboy.drawRect(0, 0, WIDTH, HEIGHT);
+    arduboy.setCursor(2, 2);
+    arduboy.print(F("MENU"));
+    arduboy.drawFastHLine(0, fh + 2, WIDTH);
 
-        for (i = 0; i < games_count; i++) {
-          arduboy.setCursor(8, fh * i + fh + fh - 1);
-          arduboy.print(games[i].name);
-        }
+    arduboy.setCursor(2, fh * choice + fh + fh - 2);
+    arduboy.print(">");
+
+    for (i = 0; i < games_count; i++) {
+      arduboy.setCursor(8, fh * i + fh + fh - 2);
+      arduboy.print(games[i].name);
+    }
     arduboy.display();
     
-
-    btns = buttonsUpdate();
-    switch (btns) {
-      case BTN_GO_UP:
+    // Handle buttons
+    if(arduboy.justPressed(UP_BUTTON)) {
         choice = (games_count + choice - 1) % games_count;
-        break;
-      case BTN_GO_DOWN:
+    }
+    if(arduboy.justPressed(DOWN_BUTTON)) {
         choice = (games_count + choice + 1) % games_count;
-        break;
-      case BTN_GO_RIGHT:
-      case BTN_GO_A:
+    }
+    if(arduboy.justPressed(RIGHT_BUTTON) || arduboy.justPressed(A_BUTTON)) {
         game_on = 1;
-        break;
     }
   }
 
-  // Reset the display driver
-  //  arduboy.initDisplay();
-  //  arduboy.setPowerSave(0);
-  //  arduboy.setContrast(contrast);
-
-  randomSeed(analogRead(A7) + millis());
+  arduboy.initRandomSeed();
 
   // READ GAME DATA
-  // TODO: EEPROM
-
+  EEPROM.get(games[choice].address, game_data);
+  if(arduboy.pressed(LEFT_BUTTON)) {
+    memset(game_data, 0, sizeof(game_data));
+  }
   // RUN THE GAME
 
   // Call game with MENU_EXIT to obtain scores
@@ -112,23 +109,24 @@ void loop() {
 
   do {
     // Show the menu
-    btns = 1;
     for (;;) {
       if (!arduboy.nextFrame()) {
         continue;
       }
       arduboy.pollButtons();
+      arduboy.clear();
+
       arduboy.drawRect(0, 0, WIDTH, HEIGHT);
       arduboy.setCursor(2, 2);
       arduboy.print(games[choice].name);
-      arduboy.drawFastHLine(0, 8, WIDTH);
-      arduboy.setCursor(2, 11);
-      arduboy.print(F(">-CTAPT"));
-      arduboy.setCursor(2, 21);
-      arduboy.print(F("^-izXOd"));
+      arduboy.drawFastHLine(0, 10, WIDTH);
+      arduboy.setCursor(2, 12);
+      arduboy.print(F(">-START"));
+      arduboy.setCursor(2, 22);
+      arduboy.print(F("^-EXIT"));
       if (game_on) {
-        arduboy.setCursor(2, 31);
-        arduboy.print(F("\\-pPOdalvi"));
+        arduboy.setCursor(2, 32);
+        arduboy.print(F("v-CONTINUE"));
       }
       arduboy.setCursor(2, 41);
       arduboy.print(score);
@@ -137,32 +135,30 @@ void loop() {
       arduboy.print(hiScore);
       arduboy.display();
 
-
-      btns = buttonsUpdate();
-      switch (btns) {
-        case BTN_GO_A:
+      // Handle buttons
+      if(arduboy.justPressed(A_BUTTON)) {
           menu = game_on ? MENU_RESUME : MENU_NEW; goto end_menu;
-        case BTN_GO_RIGHT:
-          menu = MENU_NEW; goto end_menu;
-        case BTN_GO_B:
-        case BTN_GO_UP:
-          menu = MENU_EXIT; goto end_menu;
-        case BTN_GO_DOWN:
-          if (game_on) {
-            menu = MENU_RESUME; goto end_menu;
-          }
-          break;
-        default:
-          break;
       }
+      if(arduboy.justPressed(RIGHT_BUTTON)) {
+          menu = MENU_NEW; goto end_menu;
+      }
+      if(arduboy.justPressed(UP_BUTTON) || arduboy.justPressed(B_BUTTON)) {
+          menu = MENU_EXIT; goto end_menu;
+      }
+      if(arduboy.justPressed(DOWN_BUTTON)) {
+        if (game_on) {
+          menu = MENU_RESUME; goto end_menu;
+        }
+      }
+
     }
 end_menu:
 
     // Start the game
     (*games[choice].play)(&arduboy, game_data, menu, &game_on, &score, &hiScore);
-
+    
     // SAVE GAME DATA
-    // TODO: EEPROM
+    EEPROM.put(games[choice].address, game_data);
 
   } while (menu != MENU_EXIT);
 }
