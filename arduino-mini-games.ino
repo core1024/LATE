011f0e70535074568c67446786512335cb6dab70
Vladimir Borisov <borisov1024@gmail.com>
2018-07-19 22:19:22 +0300

    Removed unused code

--- arduino-mini-games.ino
+++ arduino-mini-games.ino
@@ -3,11 +3,9 @@
 #include <EEPROM.h>
 
 #include "buttons.h"
-#include "block.h"
 
 #include "game_tetris.h"
 #include "game_1010.h"
-#include "game_bgun.h"
 
 #define BTN_PIN_UP 9
 #define BTN_PIN_DOWN 6
@@ -44,13 +42,9 @@ void setup() {
   games[1].name = F("1010!");
   games[1].address = EEPROM_STORAGE_SPACE_START + 2 * sizeof(game_data);
   games[1].play = &game1010;
-  // games[2].name = F("BGUN");
-  // games[2].address = EEPROM_STORAGE_SPACE_START;
-  // games[2].play = &gameBGun;
 
   arduboy.begin();
   arduboy.setFrameRate(30);
-  blocksSetup(&arduboy);
   buttonsSetup(&arduboy);
 
   fh = 8;
--- block.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduboy2.h>
-
-#define BLOCK_LINE 0
-
-void blocksSetup(Arduboy2 *gr);
-uint8_t blockScale(int8_t i);
-void blockDraw(int8_t x, int8_t y);
-void blockDrawFrame(void);
