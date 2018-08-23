9845697944eddb7ee9f2808d50d68821f9f6c909
Vladimir Borisov <borisov1024@gmail.com>
2018-08-23 22:15:02 +0300

    Removed extern Arduboy2 instance as it's not needed

--- game_1010.h
+++ game_1010.h
@@ -3,8 +3,6 @@
 #include <Arduboy2.h>
 #include "common.h"
 
-extern Arduboy2 arduboy;
-
 void game1010(Arduboy2 *sgr, uint8_t *gdat, uint8_t menu, uint8_t *gameOn, uint32_t *score, uint32_t *hiScore);
 const uint8_t game1010Logo[] PROGMEM = {
   0b01110111,
--- game_bb.h
+++ game_bb.h
@@ -3,8 +3,6 @@
 #include <Arduboy2.h>
 #include "common.h"
 
-extern Arduboy2 arduboy;
-
 void gameBB(Arduboy2 *sgr, uint8_t *gdat, uint8_t menu, uint8_t *gameOn, uint32_t *score, uint32_t *hiScore);
 const uint8_t gameBBLogo[] PROGMEM = {
 	0b01101110,
